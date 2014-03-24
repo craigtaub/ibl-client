a6ef351ec8ecc54511ca466618a773d8691aa439
craigtaub <craig.taub@bbc.co.uk>
2014-03-24 15:14:15 +0000

    Category model

---
+++ src/Models/Category.php
@@ -0,0 +1,46 @@
+<?php
+
+class IblClient_Models_Category extends IblClient_Models_Base implements IblClient_Models_Interface 
+{
+    public $feed = "/categories";
+
+    public $kind = "";
+
+    public $child_episode_count = 0;
+
+    public function get($params) {
+        $response = $this->_get($this->feed, $params);
+
+        $elements = $response->categories;
+        return $this->buildElements($elements);
+    }
+
+    public function buildModel($object) {
+      foreach (get_object_vars($object) as $key => $value) {
+          $this->$key = $value;
+      }
+    }
+
+    public function getKind() {
+        return $this->kind;
+    }
+
+    /**
+     * Get the number of episodes for a category
+     *
+     * @return int
+     */
+    public function getChildEpisodeCount() {
+        return $this->child_episode_count;
+    }
+
+
+    /**
+     * Returns whether this category is a children's category
+     * @return bool
+     */
+    public function isChildrens() {
+        return $this->id == 'cbbc' || $this->id == 'cbeebies';
+    }
+
+}
